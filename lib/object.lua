6858e69ac81a0357bdae67c672cb6f4ed85ca333
Dongyue Wang <codewdy@gmail.com>
2022-12-18 21:19:40 +0800

    add object

--- lib/object.lua
+++ lib/object.lua
@@ -3,14 +3,8 @@ local M = {}
 local dict_helper = require("lib.dict_helper")
 
 M.ObjectBase = {
-  fields = {
-    _init = function(self)
-    end,
-    _create = function(self)
-    end,
-    _clear = function(self)
-    end,
-  },
+  fields = {},
+  _meta = { __index = {} },
   extend = function(cls, args)
     args = dict_helper.merge({
       fields = {},
@@ -18,18 +12,53 @@ M.ObjectBase = {
     }, args)
     local rst = dict_helper.merge(cls, args.static)
     rst.fields = dict_helper.merge(rst.fields, args.fields)
+    rst._meta = { __index = rst.fields }
+    rst.base = cls
     return rst
-  end
+  end,
+  new = function(cls)
+    local rst = {}
+    setmetatable(rst, cls._meta)
+    return rst
+  end,
 }
 
 M.Object = M.ObjectBase:extend({
   fields = {
+    _init = function(self)
+    end,
+    _first_init = function(self)
+    end,
+    _clear = function(self)
+    end,
+    release = function(self)
+      self.cls:delete(self)
+    end,
   },
   static = {
     _cache = {},
-    _create = function(cls)
+    _new = function(cls)
+      if #cls._cache == 0 then
+        return M.ObjectBase:new()
+      else
+        local rst = cls._cache[#cls._cache]
+        cls._cache[#cls._cache] = nil
+        return rst
+      end
     end,
-    create = function(cls, ...)
+    new = function(cls, ...)
+      local rst = cls:_new()
+      rst:_init(...)
+    end,
+    delete = function(cls, obj)
+      obj:_clear()
+      cls._cache[#cls._cache + 1] = obj
+    end,
+    extend = function(cls, args)
+      args = dict_helper.merge({
+        static = { _cache = {} },
+      }, args)
+      return cls.base.extend(cls, args)
     end,
   },
 })
