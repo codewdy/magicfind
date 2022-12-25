54e33270335f4a122fd011c1d8a40af3154031e6
Dongyue Wang <codewdy@gmail.com>
2022-12-25 08:20:05 +0800

    fix hit test profiler

--- profiler/framework/hit_test.lua
+++ profiler/framework/hit_test.lua
@@ -6,13 +6,20 @@ ProfilerCase{
   init = function(ctx)
     math.randomseed(0)
     local units = list.List:new()
-    for i=1,1000 do
-      units:push_back(
-          { pos_x = 1 + math.random(), pos_y = 1 + math.random(), size = 1 })
+    for i=1,100 do
+      local px = (math.random() - 0.5) * i
+      local py = (math.random() - 0.5) * i
+      for j=1,10 do
+        units:push_back({
+          pos_x = px + math.random(),
+          pos_y = py + math.random(),
+          size = 1
+        })
+      end
     end
     ctx.tester = hit_test.HitTest:new(units)
   end,
   run = function(ctx)
-    ctx.tester:hit_circle(1, 1, 1)
+    ctx.tester:hit_circle(0, 0, 1)
   end,
 }
