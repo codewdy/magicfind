c77e046f3c74dc9053089cd8cc703e8e843af85a
Dongyue Wang <codewdy@gmail.com>
2023-01-02 12:54:46 +0800

    add profiler for e2e skeletom

---
+++ profiler/framework/e2e_skeleton.lua
@@ -0,0 +1,20 @@
+local Skeleton = require("fake.skeleton").Skeleton
+local Unit = require("framework.unit").Unit
+
+ProfilerCase{
+  name = "framework.e2e_skeleton",
+  init = function(ctx)
+    ctx.sketelon = {}
+    for i=1,1000 do
+      ctx.sketelon[i] = Unit:new(Skeleton, 0, 0)
+    end
+  end,
+  run = function(ctx)
+    for i=1,1000 do
+      ctx.sketelon[i]:pre_update()
+    end
+    for i=1,1000 do
+      ctx.sketelon[i]:update()
+    end
+  end,
+}
--- profiler/main.lua
+++ profiler/main.lua
@@ -4,5 +4,6 @@ require("profiler.lib.object")
 require("profiler.lib.list")
 
 require("profiler.framework.hit_test")
+require("profiler.framework.e2e_skeleton")
 
 ProfilerMain()
