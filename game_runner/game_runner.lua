613a4c52c6a021c5333630c8a9f8334dda3ee364
Dongyue Wang <codewdy@gmail.com>
2023-01-14 11:58:04 +0800

    gamerunner init

---
+++ game_runner/game_runner.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+local Object = require("lib.object").Object
+local BattleRunner = require("framework.battle_runner").BattleRunner
+local Player = require("player.player").Player
+
+M.GameRunner = Object:extend{
+  _init = function(self)
+    self.player = Player:new()
+  end,
+  new_player = function(self, filename)
+    self.filename = filename
+    self.player:new()
+  end,
+  new_battle = function(self)
+    self.battle_runner = BattleRunner:new(nil)
+    self.player.unit:set_loc(self.battle_runner, 0, 0)
+    self.battle_runner:add_unit(self.player.unit, false)
+  end,
+}
+
+return M
