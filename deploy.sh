cce03929bda7f37c5de473f90d8ad8aceceabcdf
Dongyue Wang <codewdy@gmail.com>
2023-01-14 11:34:08 +0800

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DST_DIR="$1"
 SRC_DIR=`dirname "$0"`
-rsync -a --del --exclude .git --exclude rocks "$SRC_DIR"  "$DST_DIR"
+rsync -a --del --exclude .git --exclude rocks --exclude .gitignore "$SRC_DIR"  "$DST_DIR"
